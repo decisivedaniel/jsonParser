93fb18ab82574651afef22b5197f3ddfb54c96bd
Dan <decisivedaniel@proton.me>
2024-10-30 22:02:00 +0100

    Copy over unit tests and remove attempt 1

--- json_eval.h
+++ json_eval.h
@@ -6,36 +6,7 @@
 #include <string.h>
 #include <unordered_map>
 
-class JsonBase {
-    public:
-        virtual std::string eval(std::string *parameter);
-        virtual int size(std::string *parameter);
-        virtual int min(std::string *parameter);
-        virtual int max(std::string *parameter);
-        virtual ~JsonBase();
-};
 
-class JsonObject : public JsonBase {
-
-};
-
-class JsonArray : public JsonBase {
-
-};
-
-class JsonString : public JsonBase {
-
-};
-
-class JsonNumber : public JsonBase {
-
-};
-
-class JsonFactory {
-    public:
-        JsonFactory();
-        JsonBase readJson(std::ifstream *file); 
-};
 
 
 
--- json_eval_unittest.cc
+++ json_eval_unittest.cc
@@ -1,5 +1,22 @@
 #include "json_eval.h"
 #include "gtest/gtest.h"
+
 namespace {
-    
+    class JsonTest : public testing::Test {
+        protected:
+            JsonTest() {
+                std::string testFile = "test.json";
+                std::ifstream TestFile(testFile);
+                base = readJson(&TestFile);
+                TestFile.close();
+            }
+            JsonBase base;
+    };
+
+    TEST_F(JsonTest, Trivial) {
+        EXPECT_EQ(base.eval("a.b[1]"), "2") << "a.b[1] does not return 2";
+        EXPECT_EQ(base.eval("a.b[2].c"),"test") << "a.b[2].c does not equal test";
+        EXPECT_EQ(base.eval("a.b"), '[ 1, 2, { "c" : "test" } ]');
+
+    }
 }
