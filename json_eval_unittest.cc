3cb8b827f7998765b6270247e986bad2bca387c9
Dan <decisivedaniel@proton.me>
2024-11-01 02:36:33 +0100

    Add some mailformed json tests

--- json_eval_unittest.cc
+++ json_eval_unittest.cc
@@ -11,6 +11,7 @@ namespace {
                 TestFile.close();
             }
             std::shared_ptr<Json::Node> base;
+            std::shared_ptr<Json::Node> simple;
     };
 
     TEST_F(JsonTest, Trivial) {
@@ -18,5 +19,24 @@ namespace {
         EXPECT_EQ(Json::eval(base, std::string("a.b[2].c")),std::string("test")) << "a.b[2].c does not equal test";
         EXPECT_EQ(Json::eval(base, std::string("a.b")), std::string("[ 1, 2, { \"c\" : \"test\" } ]"));
 
+    }
+
+    TEST(Exception, JsonParsing) {
+        EXPECT_THROW ({
+            try {
+                Json::readJson("doesnotexist.json");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("File doesn't exist", e.what());
+                throw;
+            }
+        }, std::invalid_argument);
+        EXPECT_THROW ({
+            try {
+                Json::readJson("invalid.json");
+            } catch (const std::exception& e) {
+                EXPECT_STREQ("Json File is malformed", e.what());
+                throw;
+            }
+        }, std::invalid_argument);
     }
 }
